2a865b931476be7d0c206ecb5244b24ee6efa65f
Jonas Geiler <git@jonasgeiler.com>
2024-05-27 09:28:00 +0200

    feat: added lua-fenster type definitions

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Lua Language Server Addon Template
-This is a template for an [addon](https://github.com/LuaLS/lua-language-server/wiki/Addons) that can be used by the [Lua Language Server](https://github.com/LuaLS/lua-language-server).
+# lua-fenster type definitions
 
-## Publishing to Addon Manager
-The [addon manager](https://github.com/LuaLS/lua-language-server/discussions/1965) for VS Code allows users to easily install and manage their addons. The addons are pulled from [LLS-Addons](https://github.com/LuaLS/LLS-Addons).
+This repository contains the type definitions for [lua-fenster](https://github.com/jonasgeiler/lua-fenster)
+that can be used by [Sumneko's Lua Language Server](https://github.com/LuaLS/lua-language-server).
 
-To add your addon to the addon manager, please read the [README for LLS-Addons](https://github.com/LuaLS/LLS-Addons#readme).
+It is a submodule for the [LuaLS/LLS-Addons](https://github.com/LuaLS/LLS-Addons) repository.
+
+## Usage
+
+Please consult the documentation for more information:
+[Installing Addons - Lua Language Server | Wiki](https://luals.github.io/wiki/addons/#installing-addons)
---
+++ library/fenster.lua
@@ -0,0 +1,111 @@
+---@meta
+--[[
+
+Type definitions for lua-fenster
+https://github.com/jonasgeiler/lua-fenster
+https://luarocks.org/modules/jonasgeiler/fenster
+
+--]]
+
+
+---@class window*
+---@field keys boolean[]
+---@field delta number
+---@field mousex integer
+---@field mousey integer
+---@field mousedown boolean
+---@field modcontrol boolean
+---@field modshift boolean
+---@field modalt boolean
+---@field modgui boolean
+---@field width integer
+---@field height integer
+---@field title string
+---@field scale integer
+---@field targetfps number
+local window = {}
+
+function window:close() end
+
+---@return boolean
+function window:loop() end
+
+---@param x integer
+---@param y integer
+---@param color integer
+function window:set(x, y, color) end
+
+---@param x integer
+---@param y integer
+---@return integer
+---@nodiscard
+function window:get(x, y) end
+
+---@param color integer?
+function window:clear(color) end
+
+---@param key string
+---@return any
+---@nodiscard
+function window:__index(key) end
+
+function window:__gc() end
+
+---@version >5.4
+function window:__close() end
+
+---@return string
+---@nodiscard
+function window:__tostring() end
+
+
+local fenster = {}
+
+---@param width integer
+---@param height integer
+---@param title string?
+---@param scale integer?
+---@param targetfps number?
+---@return window*
+---@nodiscard
+function fenster.open(width, height, title, scale, targetfps) end
+
+---@param milliseconds integer
+function fenster.sleep(milliseconds) end
+
+---@return integer
+---@nodiscard
+function fenster.time() end
+
+---@param color integer
+---@return integer, integer, integer
+---@overload fun(red: integer, green: integer, blue: integer): integer
+---@nodiscard
+function fenster.rgb(color) end
+
+---@param window window*
+function fenster.close(window) end
+
+---@param window window*
+---@return boolean
+function fenster.loop(window) end
+
+---@param window window*
+---@param x integer
+---@param y integer
+---@param color integer
+function fenster.set(window, x, y, color) end
+
+---@param window window*
+---@param x integer
+---@param y integer
+---@return integer
+---@nodiscard
+function fenster.get(window, x, y) end
+
+---@param window window*
+---@param color integer?
+function fenster.clear(window, color) end
+
+
+return fenster
